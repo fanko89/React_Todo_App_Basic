61464bfb5f001238c6ac52fc78dd5aa61685d279
derek ney <deek311@gmail.com>
2023-03-31 00:56:05 -0600

    had to move files

---
+++ src/todoApp.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+function TodoApp() {
+    const [todos, setTodos] = useState([])
+    const [newTodo, setNewTodo] = useState('')
+  
+ 
+    function addTodo() {
+      if (newTodo.trim() !== '') {
+        const newTodoItem = {
+          id: todos.length + 1,
+          text: newTodo,
+          completed: false
+        };
+        setTodos([...todos, newTodoItem])
+        setNewTodo('')
+      }
+    }
+  
+   
+    function completeTodo(id) {
+      setTodos(
+        todos.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, completed: !todo.completed }
+          }
+          return todo
+        })
+      );
+    }
+  
+    
+    function deleteTodo(id) {
+      setTodos(todos.filter((todo) => todo.id !== id))
+    }
+  
+    function editTodo(id, newText) {
+        setTodos(
+          todos.map((todo) => {
+            if (todo.id === id) {
+              return { ...todo, text: newText };
+            }
+            return todo;
+          })
+        );
+      }
+  
+    return (
+      <div>
+        <h1>Todo App</h1>
+  
+        <form
+          onSubmit={(event) => {
+            event.preventDefault()
+            addTodo();
+          }}
+        >
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(event) => setNewTodo(event.target.value)}
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => completeTodo(todo.id)}
+              />
+              {todo.completed ? <del>{todo.text}</del> : todo.text}
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+              <button
+                onClick={() =>
+                  editTodo(todo.id, prompt('Edit todo:', todo.text))
+                }
+              >
+                Edit
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default TodoApp
